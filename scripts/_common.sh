89c8dcbbac3ae5f813c337e8b778a863368ecbdc
7357-2022 <110225308+7357-2022@users.noreply.github.com>
2022-08-01 13:11:29 +0200

    Add files via upload

---
+++ scripts/_common.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#=================================================
+# COMMON VARIABLES
+#=================================================
+
+YNH_PHP_VERSION="8.0"
+
+pkg_dependencies="php${YNH_PHP_VERSION}-ldap php${YNH_PHP_VERSION}-mbstring php${YNH_PHP_VERSION}-fileinfo php${YNH_PHP_VERSION}-sqlite3 php${YNH_PHP_VERSION}-gd php${YNH_PHP_VERSION}-intl"
+
+#=================================================
+# PERSONAL HELPERS
+#=================================================
+
+#=================================================
+# EXPERIMENTAL HELPERS
+#=================================================
+
+#=================================================
+# FUTURE OFFICIAL HELPERS
+#=================================================
